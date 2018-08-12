efbe82f98001e3781ce322f31bd4b1b6255c7270
Alex Shadley <shadleyalex@gmail.com>
2018-08-12 18:41:36 -0500

    replaces membership assocation table with a relation.  Mostly semantics

--- poserrank/models.py
+++ poserrank/models.py
@@ -1,31 +1,14 @@
 from poserrank import db
 
-# This is an Association Table, which tracks the Many-to-Many relationship
-# between Users and Groups.  Many-to-Many refers to the fact that each User can
-# belong to many Groups, and that each Group can have many Users.  In the
-# in the context of database relationships, 'many' generally means 'more than
-# one'
-
-association_table = db.Table('Membership', db.Model.metadata,
-	db.Column('user_id', db.Integer, db.ForeignKey('Users.id')),
-	db.Column('group_id', db.Integer, db.ForeignKey('Groups.id'))
-)
 
 class User(db.Model):
-	__tablename__ = 'Users'
+	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(32), unique=True)
 	full_name = db.Column(db.String(64))
 	email = db.Column(db.String(64), unique=True)
 	password = db.Column(db.String(64))
 	score = db.Column(db.Integer, default=0)
-
-	# the User model's means of accessing the 'Membership' relationship; I'm not
-	# quite sure how this syntax works, see this link for more info
-	# http://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html#many-to-many
-	groups = db.relationship('Group',
-		secondary=association_table,
-		back_populates='users')
 
 #	sent_invitations = db.relationship('Invitations')
 #	recieved_invitations = db.relationship('Invitations')
@@ -41,15 +24,34 @@ class User(db.Model):
 	def __repr__(self):
 		return '<User %r>' % self.username
 
+
+class Membership(db.Model):
+	__tablename__ = 'memberships'
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.ForeignKey('users.id'), nullable=False)
+	group_id = db.Column(db.ForeignKey('groups.id'))
+	score = db.Column(db.Integer)
+
+	user = db.relationship('User', foreign_keys=[user_id])
+	group = db.relationship('Group', foreign_keys=[group_id])
+
+	def serializeable(self):
+		return {
+			'id': self.id,
+			'user_id': self.user_id,
+			'group_id': self.group_id,
+			'score': self.score,
+		}
+
+	def __repr__(self):
+		return '<Membership %r - %r>' % (self.user.username, self.group.name)
+
+
 class Group(db.Model):
-	__tablename__ = 'Groups'
+	__tablename__ = 'groups'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
 	description = db.Column(db.Text)
-
-	users = db.relationship('User',
-		secondary=association_table,
-		back_populates='groups')
 
 #	invitations = db.relationship('Invitations')
 
