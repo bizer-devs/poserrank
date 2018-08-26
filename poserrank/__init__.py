fc899ac4f45d80a08cad6fe14801d504eb7d82c0
Alex Shadley <shadleyalex@gmail.com>
2018-08-25 23:40:54 -0500

    fixes typo

--- poserrank/__init__.py
+++ poserrank/__init__.py
@@ -10,7 +10,7 @@ def app_factory(debug=False):
 	if debug:
 		app.config.from_pyfile('configs/devconfig.py')
 		try:
-			app.config.from_pyfile('config/keys.py')
+			app.config.from_pyfile('configs/keys.py')
 		except FileNotFoundError:
 			# when in debug mode, we can use some generic key if none is provided
 			app.config['SECRET_KEY'] = 'notverysecretkey'
@@ -18,7 +18,7 @@ def app_factory(debug=False):
 	else:
 		app.config.from_pyfile('configs/prodconfig.py')
 		try:
-			app.config.from_pyfile('config/keys.py')
+			app.config.from_pyfile('configs/keys.py')
 		except FileNotFoundError:
 			# in prod, not having a keyfile is unacceptable
 			raise RuntimeError('Must have a keys.py config for prod')
